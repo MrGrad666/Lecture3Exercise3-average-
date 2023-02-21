a4e91f30dd8d122f8fc7c32348e69c5939fad985
MrGrad <MrGrad@DESKTOP-T3AM2JH>
2023-02-21 21:49:48 +0300

    Add project files.

---
+++ Lecture3Exercise3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture3Exercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int c = 3;
+
+            double result = (a+b+c)/ 2;
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+
+        }
+    }
+}
